fb4068b802b02fe7bf7b5d1acf38a349efaf2a6f
manankasturia <manankasturia5@gmail.com>
2025-10-21 20:49:52 +0530

    added hover effect

--- frontend/src/components/Navbar/Center.js
+++ frontend/src/components/Navbar/Center.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Center = () => {
   return (
     <div className="w-full">
-        <ul className='flex flex-row justify-center items-center list-none w-full gap-10 '>
-            <li>Product</li>
-            <li>Use Cases</li>
-            <li>Pricing</li>
-            <li>About</li>
-        </ul>
+      <ul className="flex flex-row justify-center items-center list-none w-full gap-10 cursor-pointer">
+        <li className="bg-white font-medium hover:text-blue-600 hover:font-bold transition-all duration-1000 ease-in-out">
+          Product
+        </li>
+        <li className="bg-white font-medium hover:text-blue-600 hover:font-bold transition-all duration-1000 ease-in-out">
+          Use Cases
+        </li>
+        <li className="bg-white font-medium hover:text-blue-600 hover:font-bold transition-all duration-1000 ease-in-out">
+          Pricing
+        </li>
+        <li className="bg-white font-medium hover:text-blue-600 hover:font-bold transition-all duration-1000 ease-in-out">
+          About
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Center
+export default Center;
