d36f1adf331d0968f6cb918c224045584f068437
manankasturia <manankasturia5@gmail.com>
2025-10-22 21:41:53 +0530

    added missing files

---
+++ frontend/src/components/UseCases/UseCases.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.js";
+import Top from "./Top.js";
+import Bottom from "./Bottom.js";
+
+const UseCases = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center min-h-screen">
+      <Navbar />
+      <Top />
+      <Bottom />
+    </div>
+  );
+};
+
+export default UseCases;
