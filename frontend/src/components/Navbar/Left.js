5c03fd5237e8fcdd449a176ecfdc2f1472c85cec
manankasturia <manankasturia5@gmail.com>
2025-10-22 18:23:01 +0530

    added usecase route

--- frontend/src/components/Navbar/Left.js
+++ frontend/src/components/Navbar/Left.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import iconSrc from "../../assets/voluble_icon.png";
 
 const Left = () => {
   return (
     <>
-      <div className="flex items-center gap-3">
-        <img src={iconSrc} alt="Voluble Icon" className="w-8 h-8" />
-        <h3 className="text-xl font-bold">Voluble</h3>
-      </div>
+      <Link to="/">
+        <div className="flex items-center gap-3">
+          <img src={iconSrc} alt="Voluble Icon" className="w-8 h-8" />
+          <h3 className="text-xl font-bold">Voluble</h3>
+        </div>
+      </Link>
     </>
   );
 };
