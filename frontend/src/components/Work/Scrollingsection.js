359340a9bdec1174a8bd12c1148b2e8765fdb8a9
ROHIT-dangwal <rdsd21104@gmail.com>
2025-10-23 01:09:25 +0530

    bug fixes

--- frontend/src/components/Work/Scrollingsection.js
+++ frontend/src/components/Work/Scrollingsection.js
@@ -32,13 +32,53 @@ const Scrollingsection = () => {
 
   return (
     <div ref={scrollRef} style={{ height: SCROLL_HEIGHT }} className="relative">
+      
       <div className="sticky top-0 flex h-screen w-full items-center overflow-hidden">
         
-        {/* === THIS IS THE ONLY CHANGE === */}
-        {/* Pass the prop to Left, just like we do for Right */}
+
+        <svg
+          className="absolute top-0 left-0 w-full h-full -z-10" 
+          style={{ height: '105vh' }} 
+          fill="none"
+          viewBox="0 0 1280 643"
+          preserveAspectRatio="none"
+        >
+          <g filter="url(#lp-hero-bg_svg__a)">
+            <path
+              fill="#F6F8FF"
+              d="M1279.5 546.051s-125.75 96.547-626.142 96.547C152.962 642.598 0 541.77 0 541.77V-943.057h1280z"
+            ></path>
+          </g>
+          <defs>
+            <filter
+              id="lp-hero-bg_svg__a"
+              width="1280"
+              height="1590.65"
+              x="0"
+              y="-948.057"
+              colorInterpolationFilters="sRGB"
+              filterUnits="userSpaceOnUse"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+              <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+              <feColorMatrix
+                in="SourceAlpha"
+                result="hardAlpha"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              ></feColorMatrix>
+              <feOffset dy="-5"></feOffset>
+              <feGaussianBlur stdDeviation="10"></feGaussianBlur>
+              <feComposite in2="hardAlpha" k2="-1" k3="1" operator="arithmetic"></feComposite>
+              <feColorMatrix values="0 0 0 0 0.553024 0 0 0 0 0.599289 0 0 0 0 0.640413 0 0 0 0.1 0"></feColorMatrix>
+              <feBlend in2="shape" result="effect1_innerShadow_4512_10209"></feBlend>
+            </filter>
+          </defs>
+        </svg>
+
+
         <Left currentSlide={currentSlide} />
-        
         <Right currentSlide={currentSlide} />
+
       </div>
     </div>
   );
