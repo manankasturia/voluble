b2868e9ec2233eab0621dab9ccd5cf822a626792
manankasturia <manankasturia5@gmail.com>
2025-10-24 00:51:59 +0530

    added pricing route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar/Navbar.js";
 import Landing from "./Landing/Landing.js";
 import UseCases from "./UseCases/UseCases.js";
+import Pricing from "./Pricing/Pricing.js";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/usecases" element={<UseCases />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </Router>
   );
