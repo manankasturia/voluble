49d53a12d9d3a542a16467c1347d014dd049dba8
ROHIT-dangwal <rdsd21104@gmail.com>
2025-11-05 16:42:23 +0530

    data collection complete

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
-
+import volumeParams from './Routes/frontendRoutes/volumeParams.js';
 const port = process.env.PORT || 8080;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -11,17 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/transcript", (req, res) => {
-  const { pitchHz, volume, amplitudes, pauses, metrics } = req.body;
-  console.log("Received data: ", {
-    pitchHz,
-    volume,
-    amplitudes,
-    pauses,
-    metrics,
-  });
-  res.json({ success: true, message: "Data received successfully" });
-});
+app.use('/frontend',volumeParams);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
