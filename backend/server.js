5661434260f2185ea73cb47b909acbafaa143201
manankasturia <manankasturia5@gmail.com>
2026-02-14 15:58:22 +0530

    added backend url

--- backend/server.js
+++ backend/server.js
@@ -15,18 +15,12 @@ app.use(express.json());
 const upload = multer({ storage: multer.memoryStorage() });
 const ASSEMBLYAI_API_KEY = process.env.assembly_apikey;
 const ASSEMBLYAI_UPLOAD_URL = "https://api.assemblyai.com/v2/upload";
+const BACKEND_URL =
+  process.env.REACT_APP_API_BASE || `http://localhost:${port}`;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(`${BACKEND_URL}/frontend`, volumeParams);
 
-app.use("/frontend", volumeParams);
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.post("/upload", upload.single("audio"), async (req, res) => {
+app.post(`${BACKEND_URL}/upload`, upload.single("audio"), async (req, res) => {
   if (!req.file) {
     return res
       .status(400)
@@ -37,11 +31,15 @@ app.post("/upload", upload.single("audio"), async (req, res) => {
     const uploadResponse = await axios.post(
       ASSEMBLYAI_UPLOAD_URL,
       req.file.buffer,
-      { headers: { Authorization: ASSEMBLYAI_API_KEY } }
+      { headers: { Authorization: ASSEMBLYAI_API_KEY } },
     );
 
     res.json({ fileUrl: uploadResponse.data.upload_url });
   } catch (error) {
     res.status(500).json({ success: false, message: "Upload failed" });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
